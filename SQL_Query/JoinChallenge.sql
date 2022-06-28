80e99effcd46de65243389f782d3c4a369d4f568
SandroAma <sandroama123@gmail.com>
2022-06-28 00:37:07 -0400

    Update 2.0

--- SQL_Query/JoinChallenge.sql
+++ SQL_Query/JoinChallenge.sql
@@ -1,3 +1,20 @@
 -- California sales tax laws have changed and we need to alert our customers to this through email.
 
 -- What are the emails of the customers who live in california?
+
+SELECT district,email FROM address
+INNER JOIN customer
+ON address.address_id = customer.address_id
+WHERE district = 'California'
+
+-- Ch2
+-- A customer walks in and is a huge fan of the actor 'Nick Wahlberg' and wants to know which movies he is in.
+
+-- Get a list of all the movies "Nick Wahlberg" has been in
+
+SELECT title,first_name,last_name FROM film_actor
+INNER JOIN actor
+ON film_actor.actor_id = actor.actor_id
+INNER JOIN film
+ON film_actor.film_id = film.film_id
+WHERE first_name = 'Nick' AND last_name = 'Wahlberg'
