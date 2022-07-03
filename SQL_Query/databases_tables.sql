ad10929cbea04d8cab7dcdae6048e2c181241f54
SandroAma <sandroama123@gmail.com>
2022-07-03 10:28:36 -0400

    create database sql

---
+++ SQL_Query/databases_tables.sql
@@ -0,0 +1,11 @@
+-- Data Types:
+-- Boolean -> T or F
+-- Char -> char, varchar, and text 
+-- Numeric -> integer and floating-point number
+-- Temporal -> date, time, timestamp, and interval
+
+UUID -> Universally Unique Identifiers
+Array -> Stores an array of strings, numbers, etc.
+JSON 
+Hstore key-value pair 
+Special types such as network address and geometric Data
