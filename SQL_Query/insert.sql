a09d095d462ab03f3183f387d65d6a240cc25b20
SandroAma <sandroama123@gmail.com>
2022-07-04 15:15:37 -0400

    Create Insert functions

---
+++ SQL_Query/insert.sql
@@ -0,0 +1,18 @@
+-- -- INSERT allows to add rows in table
+
+-- General Syntax
+-- INSERT INTO table(col1,col2,...)
+-- VALUES
+-- (value1, value2,...),
+-- (value1, value2,...),...;
+INSERT into account(username, password, email, created_on)
+VALUES
+('Sandro','pass123','sandroama@gmail.com',CURRENT_TIMESTAMP)
+
+INSERT INTO job(job_name)
+VALUES
+('AI Engineer')
+
+INSERT INTO job(job_name)
+VALUES
+('President')
