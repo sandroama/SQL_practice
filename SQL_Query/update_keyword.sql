daa65a9e7ca65e5fe45f86a31669da9fde6ea109
SandroAma <sandroama123@gmail.com>
2022-07-04 22:05:54 -0400

    updating data fields in sql

--- SQL_Query/update_keyword.sql
+++ SQL_Query/update_keyword.sql
@@ -1,6 +1,52 @@
-GENERAL Syntax
-Update Table 
-SET col1 = value1
-    col2 = val2,
-WHERE 
-  condition;
+-- GENERAL Syntax
+-- Update Table 
+-- SET col1 = value1
+--     col2 = val2,
+-- WHERE 
+--   condition;
+
+
+
+Example
+
+-- Using condition
+UPDATE account
+SET last_log = CURRENT_TIMESTAMP
+where last_log IS NULL
+
+-- based on another column 
+UPDATE account
+set last_log = created_on 
+
+-- Using another table's values (update join)
+UPDATE talbeA 
+SET original_col = TableB.new_col
+FROM TableB
+WHERE tableA.id = TableB.id 
+
+
+
+-- return affected rows
+UPDATE account 
+SET last_login = created_on 
+RETURNING account_id, last_login 
+
+
+
+-- PG Admin
+UPDATE account 
+SET last_log = CURRENT_TIMESTAMP
+
+UPDATE account
+SET last_log = created_on
+
+UPDATE account_job
+SET hire_date = account.created_on
+FROM account
+WHERE account_job.user_id = account.user_id
+
+
+-- No need for: SELECT * FROM account
+UPDATE account
+SET last_log = CURRENT_TIMESTAMP
+RETURNING email, created_on, last_log
